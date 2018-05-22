26e9cf1a348c825a1bf73cbf793941323172a23a
Alexis Michaltsis <alexis@michaltsis.net>
2018-05-22 17:58:20 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pycorset',
+    version='1.0.0',
+    description='Yet another Python starter',
+    url="https://github.com/4lm/pycorset",
+    author="Alexis Michaltsis",
+    author_email="alexis@michaltsis.net",
+    long_description=readme,
+    license=license,
+    install_requires=[
+    'sphinx'
+    ],
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        'console_scripts': [
+            'pycorset = pycorset.__main__:main'
+        ]
+    })
